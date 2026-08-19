c56749a0ae193149eaa79e7f88e648d686ef261e
Mortis66666 <tanzhiheng86@gmail.com>
2026-08-19 19:18:11 +0800

    better footer

--- footer.php
+++ footer.php
@@ -1,3 +1,41 @@
-<footer class="footer" id="footer">
-    <div>Fake footer lol</div>
+<footer class="footer">
+
+    <div class="footer-brand">
+        <div class="footer-logo">
+            LOGO
+        </div>
+
+        <div class="footer-title">
+            Treenity APU
+        </div>
+    </div>
+
+
+    <div class="footer-info">
+        <div class="footer-contact">
+            <span>Contact Us</span>
+            <span class="divider"></span>
+            <a href="tel:012-3456789"><span>012-3456789</span></a>
+            <span class="divider"></span>
+            <a href="mailto:treenityapu@gmail.com"><span>treenityapu@gmail.com</span></a>
+        </div>
+
+        <div class="footer-copyright">
+            Copyright © 2026 Treenity APU
+        </div>
+    </div>
+
+
+    <div class="footer-socials" aria-label="Social media links">
+        <a class="social-link social-facebook" href="https://www.facebook.com/" target="_blank" rel="noopener" aria-label="Facebook">
+            <img src="images/assets/facebook.svg" alt="">
+        </a>
+        <a class="social-link social-instagram" href="https://www.instagram.com/" target="_blank" rel="noopener" aria-label="Instagram">
+            <img src="images/assets/instagram.svg" alt="">
+        </a>
+        <a class="social-link social-github" href="https://github.com/Mortis66666/treenity" target="_blank" rel="noopener" aria-label="GitHub">
+            <img src="images/assets/github.svg" alt="">
+        </a>
+    </div>
+
 </footer>
--- global.php
+++ global.php
@@ -1,8 +1,7 @@
 <?php
-header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', FALSE);
-header('Pragma: no-cache');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 ?>
 
 <link rel="stylesheet" href="styles/global.css">
